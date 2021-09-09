f46539d0f2a02391a3db876386da05074aae0096
theodorsm <theodor@midtlien.com>
2021-09-09 15:20:23 +0200

    Add PDF and CDF, remove multiple sample support

--- make_plot.py
+++ make_plot.py
@@ -5,6 +5,7 @@ import math
 import os
 import sys
 from collections import Counter
+from scipy.integrate import cumtrapz
 
 import matplotlib.pyplot as plt
 import numpy as np
@@ -31,98 +32,66 @@ def parse_file(path):
     }
 
 
-def calc_SD(values, N, mean):
-    SS = sum(list(map(lambda value: (value - mean) ** 2, values)))
-    return math.sqrt(SS / (N - 1))
-
-
-def calc_conf(sigma, N):
-    return 1.960 * (sigma / math.sqrt(N))
-
-
 def main():
-    # 1. arg: filename to save to.
-    # 2. arg: number of samples
-
     filename = str(sys.argv[1])
-    N = int(sys.argv[2])
-    paths = sys.argv[3 : 3 + N]
-    data = []
-    mean_samples = []
-    max_samples = []
-    min_samples = []
-    jitter_samples = []
-    tot_histogram = {}
+    path = sys.argv[2]
+    sample = parse_file(path)
+    mean = sample["mean"]
+    tot_max = sample["total_max"]
+    tot_min = sample["total_min"]
+    jitter = sample["jitter"]
+    histogram = sample["histogram"]
 
-    for path in paths:
-        sample = parse_file(path)
-        data.append(sample)
-        mean_samples.append(sample["mean"])
-        max_samples.append(sample["total_max"])
-        min_samples.append(sample["total_min"])
-        jitter_samples.append(sample["jitter"])
-        h = sample["histogram"]
-        tot_histogram = Counter(tot_histogram) + Counter(h)
-
-    print(tot_histogram)
-    lists = sorted(tot_histogram.items(), key=lambda point: int(point[0]))
+    print(histogram)
+    lists = sorted(histogram.items(), key=lambda point: int(point[0]))
     x, y = zip(*lists)
     total = sum(y)
     print("x:", x)
     print("y:", y)
     print(total)
-    print("jitter", jitter_samples)
+    print("jitter", jitter)
     iterator = map(lambda value: (value / total) * 100, y)
     y = list(iterator)
 
-    mean = sum(mean_samples) / N
-    total_max = sum(max_samples) / N
-    total_min = sum(min_samples) / N
-    jitter = sum(jitter_samples) / N
+    fig, ax1 = plt.subplots(ncols=1)
+    ax2 = ax1.twinx()
 
-    mean_SD = calc_SD(mean_samples, N, mean)
-    total_max_SD = calc_SD(max_samples, N, total_max)
-    total_min_SD = calc_SD(min_samples, N, total_min)
-    jitter_SD = calc_SD(jitter_samples, N, jitter)
-
-    mean_conf = calc_conf(mean_SD, N)
-    total_max_conf = calc_conf(total_max_SD, N)
-    total_min_conf = calc_conf(total_min_SD, N)
-    jitter_conf = calc_conf(jitter_SD, N)
-
-    fig, ax = plt.subplots()
     textstr = "\n".join(
         (
-            r"$\mathrm{mean}=%.2f$ +/- %.2f" % (mean, mean_conf),
-            r"$\mathrm{max}=%.2f$ +/- %.2f" % (total_max, total_max_conf),
-            r"$\mathrm{min}=%.2f$ +/- %.2f" % (total_min, total_min_conf),
-            r"$\mathrm{jitter}=%.2f$ +/- %.2f" % (jitter, jitter_conf),
+            r"$\mathrm{mean}=%.2f$" % (mean),
+            r"$\mathrm{max}=%.2f$" % (tot_max),
+            r"$\mathrm{min}=%.2f$" % (tot_min),
+            r"$\mathrm{jitter}=%.2f$" % (jitter),
         )
     )
 
-    # these are matplotlib.patch.Patch properties
     props = dict(boxstyle="round", facecolor="wheat", alpha=0.5)
-    # place a text box in upper left in axes coords
-    ax.text(
+
+    plt.title("PDF and CDF for one run")
+    ax1.text(
         0.05,
         0.95,
         textstr,
-        transform=ax.transAxes,
+        transform=ax1.transAxes,
         fontsize=11,
         verticalalignment="top",
         bbox=props,
     )
 
-    # Plot values
-    plt.bar(x, y, color="b")
+    ax1.bar(x, y, color="b", label="PDF")
+    ax1.set_xlabel("Latency in us")
+    ax1.set_ylabel("% of packets")
+    ax1.legend(bbox_to_anchor=(0.95, 0.8))
+    ax1.tick_params(labelrotation=90)
 
-    # Uncomment for grid
-    # plt.grid(color="r", linestyle=":", linewidth=1)
+    cdf = cumtrapz(x=np.array(list(map(lambda val: int(val), x))), y=np.array(y))
+    cdf = cdf / max(cdf)
 
-    plt.xlabel("Latency in ms")
-    plt.ylabel("% of packets")
-    # Plot the average line
-    plt.savefig("./plots/" + filename + ".png")
+    ax2.step(x[1:], cdf, color="r", label="CDF")
+    ax2.set_ylabel("Cumulative prob.")
+    ax2.legend(bbox_to_anchor=(0.95, 0.7))
+
+    plt.savefig("./plots/" + filename + ".png", bbox_inches="tight")
 
 
 if __name__ == "__main__":
